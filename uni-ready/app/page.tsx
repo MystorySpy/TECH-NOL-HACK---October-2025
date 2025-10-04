d40cc1b95fee9bb3e8641d0760483bf3ef8f49e9
NirajMalokar21 <nirajmalokar@gmail.com>
2025-10-04 15:04:46 -0400

    Add Sign-up and Sign-in pages

--- uni-ready/app/page.tsx
+++ uni-ready/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
+import { SignOutButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
+  
 
   return (
-    <div>
-      Hello
+    <div>     
+      <h1>Welcome to Uni-Ready</h1>
     </div>
   );
 }
